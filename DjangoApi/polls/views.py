0d00ee40afc5f33f584f9c06d4426ed103fabe6c
themba ngulube <thembangulube57@gmail.com>
2025-02-26 20:00:37 +0200

    added url endpoints

--- DjangoApi/polls/views.py
+++ DjangoApi/polls/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Questions
 
 # Create your views here.
 def index(request) :
-    return HttpResponse("this is the polls application")
+    latest_question_list = Questions.objects.order_by("pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id): 
     return HttpResponse("You're looking at a question %s" % question_id)
 
 
 def results(request, question_id): 
-    response = "you're looking at the results od question %s"
+    response = "you're looking at the results of question %s"
     return HttpResponse(response,question_id)
 
 def vote(request, question_id):
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # django-API
-dajng api backend
+django api backend
